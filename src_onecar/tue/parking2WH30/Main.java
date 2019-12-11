b1f4b4322c7c428bc32b00e3151072482030a077
Corneille <n.corneille@student.tue.nl>
2019-12-11 10:58:38 +0100

    kaulo wilde flex neef

--- src_onecar/tue/parking2WH30/Main.java
+++ src_onecar/tue/parking2WH30/Main.java
@@ -2,7 +2,7 @@ package tue.parking2WH30;
 
 public class Main {
 	public SimulationHandler sim;
-	public final int N = 1000;
+	public final int N = 10;
 	
 	public static void main(String... args) {
 		Main m = new Main();
@@ -11,7 +11,10 @@ public class Main {
 		
 		double distance = 0;
 		for(int i = 0; i < m.N; i++) {
-			distance += m.sim.updateReturnDistance();
+			double d = m.sim.updateReturnDistance();
+			distance += d;
+			//System.out.println(d);
+			m.sim.printParkingLot();
 		}
 		distance /= (double)m.N;
 		System.out.println(distance);
--- src_onecar/tue/parking2WH30/SimulationHandler.java
+++ src_onecar/tue/parking2WH30/SimulationHandler.java
@@ -4,31 +4,36 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class SimulationHandler {
-	private boolean[] parkingLot;
+	private boolean[] parkingLot = {false, false, true, true};
 	
 	private final double[] drivingDistances = {1,2,3,4};
-	private final double[] walkingDistances = {5,6,7,8};
+	private final double[] walkingDistances = {2,2.5,1.5,1};
 	
 	private final int strategyNumber = 1;
+	
+	private ArrayList<Integer> occupiedSpotIndices;
 	
 	
 	private Random random;
 		
 	public SimulationHandler() {
-		parkingLot = new boolean[4];
+		//parkingLot = new boolean[4];
+		occupiedSpotIndices = new ArrayList<Integer>();
+		random = new Random();
 		
-		// TODO: init parkingLot randomly
-	}
-	
-	private void emptyRandomParkingSpot() {
-		ArrayList<Integer> occupiedSpotIndices = new ArrayList<Integer>();
 		for(int i = 0; i < parkingLot.length; i++) {
 			if(parkingLot[i]) {
 				occupiedSpotIndices.add(i);
 			}
 		}
 		
-		parkingLot[occupiedSpotIndices.get(random.nextInt(occupiedSpotIndices.size()))] = false;
+		// TODO: init parkingLot randomly
+	}
+	
+	private void emptyRandomParkingSpot() {
+		int index = random.nextInt(occupiedSpotIndices.size());
+		parkingLot[occupiedSpotIndices.get(index)] = false;
+		occupiedSpotIndices.remove(index);
 	}
 	
 	public double updateReturnDistance() {
@@ -40,6 +45,8 @@ public class SimulationHandler {
 				if(!parkingLot[i]) {
 					distance = drivingDistances[i] + walkingDistances[i];
 					parkingLot[i] = true;
+					occupiedSpotIndices.add(i);
+					break;
 				}
 			}
 		break;
@@ -48,5 +55,16 @@ public class SimulationHandler {
 		}
 		emptyRandomParkingSpot();
 		return distance;
+	}
+	
+	//temporary bruh	
+	public void printParkingLot() {
+		System.out.println(boolToInt(parkingLot[0]) + "" + boolToInt(parkingLot[1]));
+		System.out.println(boolToInt(parkingLot[2]) + "" + boolToInt(parkingLot[3]));
+		System.out.println();
+	}
+	
+	private int boolToInt(boolean b) {
+		return b ? 1 : 0;
 	}
 }
