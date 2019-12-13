c0c7473a7bc819f54e9dc0b9007b9595ddf14c42
Noé Corneille <noecorneille@gmail.com>
2019-12-13 16:36:45 +0100

    Added file i/o and strat 2

--- src_onecar/tue/parking2WH30/SimulationHandler.java
+++ src_onecar/tue/parking2WH30/SimulationHandler.java
@@ -1,21 +1,26 @@
 package tue.parking2WH30;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Locale;
 import java.util.Random;
+import java.util.Scanner;
 
 public class SimulationHandler {
-	private boolean[] parkingLot = {false, false, true, true};
+	private boolean[] parkingLot;
 	
-	private final double[] drivingDistances = {1,2,3,4};
-	private final double[] walkingDistances = {2,2.5,1.5,1};
+	private double[] drivingDistances;
+	private double[] walkingDistances;;
+	private final double parkingLotLength = 5;
 	
 	private final double drivespeed = 4;
 	private final double walkspeed = 1;
 	
 	private final double occupancyRate = 0.5;
 	
-	private final int strategyNumber = 1;
+	private final int strategyNumber = 3;
 	
 	private ArrayList<Integer> occupiedSpotIndices;
 	
@@ -29,7 +34,25 @@ public class SimulationHandler {
 	}
 	
 	private void initParkingSpace(double probability) {
-		parkingLot = new boolean[4];
+		try {
+			Scanner s = new Scanner(new File("drivingDistances.txt")).useLocale(Locale.US);
+			drivingDistances = new double[100];
+			for(int i = 0; i < drivingDistances.length; i++) {
+				drivingDistances[i] = s.nextDouble();
+			}
+			
+			s = new Scanner(new File("walkingDistances.txt")).useLocale(Locale.US);
+			walkingDistances = new double[100];
+			for(int i = 0; i < walkingDistances.length; i++) {
+				walkingDistances[i] = s.nextDouble();
+			}
+			
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+		parkingLot = new boolean[100];
 		for(int i = 0; i < parkingLot.length; i++) {
 			parkingLot[i] = false;
 		}
@@ -67,20 +90,36 @@ public class SimulationHandler {
 				}
 			}
 		break;
+		case 2:
+			for(int i = parkingLot.length-1; i >= 0; i--) {
+				if(!parkingLot[i]) {
+					distance = (drivingDistances[i] + parkingLotLength)/drivespeed + walkingDistances[i]/walkspeed;
+					parkingLot[i] = true;
+					occupiedSpotIndices.add(i);
+					break;
+				}
+			}
+		break;
 		// TODO: Rest of strategies
 		default:
 		break;		
 		}
+		System.out.print("aAAAAAAAAAAAAAAAA\n");
 		emptyRandomParkingSpot();
+		System.out.print("aAAAAAAAAAAAAAAAA\n");
 		return distance;
 	}
 	
 	// TODO: data vis
 	//temporary bruh	
 	public void printParkingLot() {
-		System.out.println(boolToInt(parkingLot[0]) + "" + boolToInt(parkingLot[1]));
-		System.out.println(boolToInt(parkingLot[2]) + "" + boolToInt(parkingLot[3]));
-		System.out.println();
+		for(int i = 0; i < 10; i++) {
+			for(int j = 0; j < 10; j++) {
+				System.out.print(boolToInt(parkingLot[i+j]));
+			}
+			System.out.print("\n");
+		}
+		System.out.print("\n");
 	}
 	
 	private int boolToInt(boolean b) {
